370ca8e58e01ac1877f97981f10f6d91903b3aef
pchwow <pchwow@naver.com>
2023-02-16 22:19:51 +0900

    kosta master commit

--- Kosta/src/KostaGit.java
+++ Kosta/src/KostaGit.java
@@ -4,5 +4,6 @@ public class KostaGit {
 	public static void main(String[] args) {
 		System.out.println("Kosta-Git");
 		System.out.println("Kosta-Git1");
+		System.out.println("Kosta-Git2");
 	}
 }
